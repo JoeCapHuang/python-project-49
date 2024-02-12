3e9d8030372d91516783dea614bae00ece719300
JoeCapHuang <kapt.tesla@gmail.com>
2024-02-12 12:50:49 +0300

    move and rename brain_games/games/game_engine.py to brain_games/engine.py

---
+++ brain_games/engine.py
@@ -0,0 +1,30 @@
+import sys
+import prompt
+
+
+def welcome_user():
+    print("Welcome to the Brain Games!")
+    name = prompt.string('May I have you name? ')
+    print(f'Hello, {name}!')
+    return name
+
+
+def start_game(game):
+    name = welcome_user()
+
+    print(game.get_task())
+
+    for i in range(3):
+        exercise = game.get_exercise()
+        print(f'Question: {exercise}')
+        answer = prompt.string('Your answer: ')
+        correct_answer = str(game.get_correct_answer(exercise))
+
+        if correct_answer == answer:
+            print('Correct!')
+
+        else:
+            print(f"{answer} is wrong answer ;(. Correct answer was {correct_answer}. Let's try again, {name}!")
+            sys.exit()
+
+    print(f'Congratulations, {name}!')
