1975c691d5970895d2550e6f3e45af7307a695be
JoeCapHuang <kapt.tesla@gmail.com>
2024-02-15 17:13:39 +0300

    update game_gcd.py

--- brain_games/games/game_gcd.py
+++ brain_games/games/game_gcd.py
@@ -1,5 +1,5 @@
 from random import randint
-
+from math import gcd
 
 TASK = 'Find the greatest common divisor of given numbers.'
 
@@ -7,16 +7,8 @@ TASK = 'Find the greatest common divisor of given numbers.'
 def get_exercise_and_answer():
     nums = randint(1, 100), randint(1, 100)
     first_number, second_number = nums
+
     exercise = ' '.join(map(str, nums))
-
-    while first_number != 0 and second_number != 0:
-
-        if first_number > second_number:
-            first_number = first_number % second_number
-
-        else:
-            second_number = second_number % first_number
-
-    answer = first_number + second_number
+    answer = gcd(first_number, second_number)
 
     return exercise, answer
