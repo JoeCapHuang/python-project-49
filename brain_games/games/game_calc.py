5b6aeaa3e53527bc4d435b35a9a192f576796daa
JoeCapHuang <kapt.tesla@gmail.com>
2024-02-19 13:15:52 +0300

    update game_calc.py

--- brain_games/games/game_calc.py
+++ brain_games/games/game_calc.py
@@ -4,14 +4,6 @@ import random
 TASK = 'What is the result of the expression?'
 
 
-def get_random(unit: str) -> int:
-    if unit == 'num':
-        return random.randint(1, 50)
-
-    elif unit == 'operator':
-        return random.choice(["+", "-", "*"])
-
-
 def get_exercise_and_answer():
     exercise = (random.randint(1, 50), random.choice(["+", "-", "*"]),
                 random.randint(1, 50))
