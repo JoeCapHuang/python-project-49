e99e94628e5343772762b2562f9518cc22e3a627
JoeCapHuang <kapt.tesla@gmail.com>
2024-02-20 19:06:51 +0300

    update game_calc.py

--- brain_games/games/game_calc.py
+++ brain_games/games/game_calc.py
@@ -5,17 +5,20 @@ TASK = 'What is the result of the expression?'
 
 
 def get_exercise_and_answer():
-    exercise = (random.randint(1, 50), random.choice(["+", "-", "*"]),
-                random.randint(1, 50))
+    first_number = random.randint(1, 50)
+    second_number = random.randint(1, 50)
+    operator = random.choice(["+", "-", "*"])
+
+    exercise = (first_number, operator, second_number)
     answer = None
 
-    if exercise[1] == '+':
-        answer = exercise[0] + exercise[2]
+    if operator == '+':
+        answer = first_number + second_number
 
-    elif exercise[1] == '-':
-        answer = exercise[0] - exercise[2]
+    elif operator == '-':
+        answer = first_number - second_number
 
-    elif exercise[1] == '*':
-        answer = exercise[0] * exercise[2]
+    elif operator == '*':
+        answer = first_number * second_number
 
     return ' '.join(map(str, exercise)), answer
